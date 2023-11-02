81efc21da59bf2a8175b628622dbca19a2dbd631
danielp1218 <danielpu2007@gmail.com>
2023-11-02 17:38:14 -0400

    Add PurgeCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import { purgeCss } from "vite-plugin-tailwind-purgecss";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		purgeCss({
+			safelist: {
+				// any selectors beginning with "hljs-" will not be purged
+				greedy: [/^hljs-/]
+			}
+		})
+	],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	}
