5feba9c60fc9ce8abb692fd647dda6ce9dd35933
Skilled5041 <skilled5041@gmail.com>
2023-11-04 15:30:01 -0400

    Add supabase

---
+++ src/lib/supabase/database.types.ts
@@ -0,0 +1,59 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+
+export interface Database {
+	public: {
+		Tables: {
+			problems: {
+				Row: {
+					code: string;
+					created_at: string;
+					group: string;
+					id: number;
+					is_organization_private: boolean;
+					is_public: boolean;
+					name: string;
+					partial: boolean;
+					points: number;
+					types: string[];
+				};
+				Insert: {
+					code: string;
+					created_at?: string;
+					group: string;
+					id?: number;
+					is_organization_private: boolean;
+					is_public: boolean;
+					name: string;
+					partial: boolean;
+					points: number;
+					types: string[];
+				};
+				Update: {
+					code?: string;
+					created_at?: string;
+					group?: string;
+					id?: number;
+					is_organization_private?: boolean;
+					is_public?: boolean;
+					name?: string;
+					partial?: boolean;
+					points?: number;
+					types?: string[];
+				};
+				Relationships: [];
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			[_ in never]: never;
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
+}
---
+++ src/lib/supabase/supabase.ts
@@ -0,0 +1,8 @@
+import { SECRET_SUPABASE_SERVICE_ROLE_KEY, SECRET_SUPABASE_URL } from "$env/static/private";
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "$lib/supabase/database.types";
+
+export const supabase = createClient<Database>(
+	SECRET_SUPABASE_URL,
+	SECRET_SUPABASE_SERVICE_ROLE_KEY
+);
